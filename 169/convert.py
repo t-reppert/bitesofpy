868de04d55cb3d4f3c1d9a6abe09d9327c4477bd
TR <thomas@reppert.info>
2019-05-16 21:37:32 -0400

    submission Bite 169 @ codechalleng.es

---
+++ 169/convert.py
@@ -0,0 +1,18 @@
+def convert(value: float, fmt: str) -> float:
+    """Converts the value to the designated format.
+
+    :param value: The value to be converted must be numeric or raise a TypeError
+    :param fmt: String indicating format to convert to
+    :return: Float rounded to 4 decimal places after conversion
+    """
+    if type(value) == float or type(value) == int:
+        if fmt == "cm" or fmt == "in":
+            if fmt =="cm":
+                return value / 0.39
+            else:
+                return 0.39 * value
+        else:
+            raise ValueError
+    else:
+        raise TypeError
+
