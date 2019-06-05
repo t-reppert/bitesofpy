7bd31cb2bb07c798edcf0e2359b458c6c17a332e
TR <thomas@reppert.info>
2019-06-05 09:37:30 -0400

    submission Bite 138 @ codechalleng.es

--- 138/zoo.py
+++ 138/zoo.py
@@ -1,16 +1,16 @@
 import itertools
 class Animal:
-    count = itertools.count(start=10001)
-    animals = {}
+    _count = itertools.count(start=10001)
+    _animals = []
 
     def __init__(self, name):
         self.name = name.title()
-        self.id = next(self.count)
-        self.animals[self.id] = self.name
+        self.id = next(self._count)
+        self._animals.append(self)
 
     def __str__(self):
         return f'{self.id}. {self.name}'
     
     @classmethod
     def zoo(cls):
-        return [f'{i}. {n}' for i,n in cls.animals.items()]
+        return '\n'.join([str(animal) for animal in cls._animals])
