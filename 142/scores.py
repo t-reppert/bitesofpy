97f055368cf2dd114fef8e3ee2291d2f5f59f705
TR <thomas@reppert.info>
2019-06-15 10:18:17 -0400

    submission Bite 142 @ codechalleng.es

---
+++ 142/scores.py
@@ -0,0 +1,50 @@
+from collections import namedtuple
+
+MIN_SCORE = 4
+DICE_VALUES = range(1, 7)
+
+Player = namedtuple('Player', 'name scores')
+
+
+def calculate_score(scores):
+    """Based on a list of score ints (dice roll), calculate the
+       total score only taking into account >= MIN_SCORE
+       (= eyes of the dice roll).
+
+       If one of the scores is not a valid dice roll (1-6)
+       raise a ValueError.
+
+       Returns int of the sum of the scores.
+    """
+    total = 0
+    for val in scores:
+        val = int(val)
+        if val not in DICE_VALUES:
+            raise ValueError
+        if val >= MIN_SCORE:
+            total += val
+    return total
+
+
+def get_winner(players):
+    """Given a list of Player namedtuples return the player
+       with the highest score using calculate_score.
+
+       If the length of the scores lists of the players passed in
+       don't match up raise a ValueError.
+
+       Returns a Player namedtuple of the winner.
+       You can assume there is only one winner.
+
+       For example - input:
+         Player(name='player 1', scores=[1, 3, 2, 5])
+         Player(name='player 2', scores=[1, 1, 1, 1])
+         Player(name='player 3', scores=[4, 5, 1, 2])
+
+       output:
+         Player(name='player 3', scores=[4, 5, 1, 2])
+    """
+    for player in players:
+        if len(player.scores) != len(players[0].scores):
+            raise ValueError
+    return max(players,key=lambda x:calculate_score(x.scores))
