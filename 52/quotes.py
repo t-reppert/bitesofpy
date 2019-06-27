f8d6e2f0a90018a2e38769e9a14520ab351b6a57
TR <thomas@reppert.info>
2019-06-27 12:30:12 -0400

    submission Bite 52 @ codechalleng.es

--- 52/quotes.py
+++ 52/quotes.py
@@ -23,45 +23,42 @@ quotes = [
 
 def _get_quote(qid):
     """Recommended helper"""
-    for quote in quotes:
-        if qid == quote['id']:
-            return [quote]
+    return [quote for quote in quotes if qid == quote['id']]
 
 
 def _quote_exists(existing_quote):
     """Recommended helper"""
-    for i in range(len(quotes)):
-        if existing_quote == quotes[i]['id']:
-            return True
-    return False
+    return [ quote for quote in quotes if quote['quote'] == existing_quote ]
 
 
 @app.route('/api/quotes', methods=['GET'])
 def get_quotes():
-    return jsonify({"quotes": quotes, "status_code": 200})
+    return jsonify({"quotes": quotes})
 
 
 @app.route('/api/quotes/<int:qid>', methods=['GET'])
 def get_quote(qid):
-    if _quote_exists(qid):
-        quote = _get_quote(qid)
-        return jsonify({'quotes':quote,'status_code':200})
-    abort(404)
+    quote = _get_quote(qid)
+    if not quote:
+        abort(404)
+
+    return jsonify({'quotes':quote})
 
 
 @app.route('/api/quotes', methods=['POST'])
 def create_quote():
     if not request.json:
         abort(400)
-    if not request.get_json().get('quote') or not request.get_json().get('movie'):
+    quote = request.json.get('quote')
+    movie = request.json.get('movie')
+    if not quote or not movie:
         abort(400)
-    req = request.json
-    quote = req['quote']
-    movie = req['movie']
+    if _quote_exists(quote):
+        abort(400)
     for i in range(len(quotes)):
         if quotes[i]['quote'] == quote and quotes[i]['movie'] == movie:
             abort(400)
-    new_id = len(quotes) + 1
+    new_id = quotes[-1].get("id") + 1
     new_quote = {"id":new_id,"quote":quote,"movie":movie}
     quotes.append(new_quote)
     return jsonify({"quote":new_quote}), 201
@@ -71,23 +68,25 @@ def create_quote():
 def update_quote(qid):
     if not request.json:
             abort(400)
-    if _quote_exists(qid):
-        req = request.json
-        quote = req['quote']
-        movie = req['movie']
-        for i in range(len(quotes)):
-            if qid == quotes[i]['id']:
-                if quotes[i]['quote'] == quote and quotes[i]['movie'] == movie:
-                    return jsonify({'quote':quotes[i]}),200
-                quotes[i]['quote'] = quote
-                quotes[i]['movie'] = movie
+    
+    if len(_get_quote(qid)) != 1:
+        abort(404)
+        
+    req = request.json
+    quote = req['quote']
+    movie = req['movie']
+    for i in range(len(quotes)):
+        if qid == quotes[i]['id']:
+            if quotes[i]['quote'] == quote and quotes[i]['movie'] == movie:
                 return jsonify({'quote':quotes[i]}),200
-    abort(404)
+            quotes[i]['quote'] = quote
+            quotes[i]['movie'] = movie
+            return jsonify({'quote':quotes[i]}),200
 
 
 @app.route('/api/quotes/<int:qid>', methods=['DELETE'])
 def delete_quote(qid):
-    if _quote_exists(qid):
+    if len(_get_quote(qid)) == 1:
         for i in range(len(quotes)):
             if quotes[i]['id'] == qid:
                 del quotes[i]
