95e59f811a54dea0eb8c77e1087bf94b65b4f4f4
TR <thomas@reppert.info>
2019-06-29 17:55:00 -0400

    submission Bite 123 @ codechalleng.es

--- 123/friends.py
+++ 123/friends.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 names = 'bob julian tim martin rod sara joyce nick beverly kevin'.split()
 ids = range(len(names))
 users = dict(zip(ids, names))  # 0: bob, 1: julian, etc
@@ -11,17 +12,12 @@ def get_friend_with_most_friends(friendships=friendships):
     """Receives the friendships list of user ID pairs,
        parse it to see who has most friends, return a tuple
        of (name_friend_with_most_friends, his_or_her_friends)"""
-    friends = {}
+    friends = defaultdict(set)
     for f1, f2 in friendships:
         friend1 = users[f1]
         friend2 = users[f2]
-        if friend1 in friends:
-            friends[friend1].append(friend2)
-        else:
-            friends[friend1] = [friend2]
-        if friend2 in friends:
-            friends[friend2].append(friend1)
-        else:
-            friends[friend2] = [friend1]
+        friends[friend1].add(friend2)
+        friends[friend2].add(friend1)
+
     friend_with_most = sorted(friends,key=lambda k:len(friends[k]),reverse=True)[0]
     return (friend_with_most,friends[friend_with_most])
