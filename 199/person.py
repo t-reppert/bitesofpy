8c724d1cd45e45e7864411951a6c7fc899ec3698
TR <thomas@reppert.info>
2019-06-29 10:58:35 -0400

    submission Bite 199 @ codechalleng.es

---
+++ 199/person.py
@@ -0,0 +1,15 @@
+class Person(object):
+    def __str__(self):
+        return 'I am a person'
+
+class Father(Person):
+    def __str__(self):
+        return super().__str__() + ' and cool daddy'
+
+class Mother(Person):
+    def __str__(self):
+        return super().__str__() + ' and awesome mom'
+
+class Child(Father,Mother):
+    def __str__(self):
+        return 'I am the coolest kid'
