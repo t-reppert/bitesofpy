4a48d16b12c53a30173d87122b6095a04329575e
TR <thomas@reppert.info>
2019-07-01 09:51:33 -0400

    submission Bite 166 @ codechalleng.es

---
+++ 166/ini.py
@@ -0,0 +1,31 @@
+import configparser
+import re
+
+class ToxIniParser:
+
+    def __init__(self, ini_file):
+        """Use configparser to load ini_file into self.config"""
+        self.config = configparser.ConfigParser()
+        self.config.read(ini_file)
+
+    @property
+    def number_of_sections(self):
+        """Return the number of sections in the ini file.
+           New to properties? -> https://pybit.es/property-decorator.html
+        """
+        return len(self.config.sections())
+
+    @property
+    def environments(self):
+        """Return a list of environments
+           (= "envlist" attribute of [tox] section)"""
+        values = re.split(',|\n', self.config['tox']['envlist'])
+        new_vals = [ x.strip() for x in values if x]
+        return sorted(new_vals)
+
+    @property
+    def base_python_versions(self):
+        """Return a list of all basepython across the ini file"""
+        return { self.config[section]['basepython'] for 
+                 section in self.config.sections() 
+                 if 'basepython' in self.config[section] }
