9c14c7ce04af21b870ba8ca2e747da2a987883d7
TR <thomas@reppert.info>
2019-07-02 18:38:40 -0400

    submission Bite 196 @ codechalleng.es

--- 196/js.py
+++ 196/js.py
@@ -9,26 +9,6 @@ class JsObject(dict):
        Try this on a regular dict and you get
        AttributeError: 'dict' object has no attribute 'foo'
     """
-    def __init__(self,*arg,**kwargs):
-        self.__dict__.update(*arg,**kwargs)
-        
-    def __setitem__(self, key, item):
-        self.__dict__[key] = item
-
-    def __getitem__(self, key):
-        return self.__dict__[key]
-
-    def __len__(self):
-        return len(self.__dict__)
-
-    def __delitem__(self, key):
-        del self.__dict__[key]
-
-    def update(self, *args, **kwargs):
-        return self.__dict__.update(*args, **kwargs)
-
-    def keys(self):
-        return self.__dict__.keys()
-
-    def values(self):
-        return self.__dict__.values()
+    def __init__(self, *arg, **kwargs):
+        super().__init__(self, *arg, **kwargs)
+        self.__dict__ = self
