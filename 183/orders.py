cddf0ce379934e7470fa27f682ea9b3880adac50
TR <thomas@reppert.info>
2019-07-02 09:40:53 -0400

    submission Bite 183 @ codechalleng.es

---
+++ 183/orders.py
@@ -0,0 +1,44 @@
+from os import path
+from urllib.request import urlretrieve
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+EXCEL = path.join('/tmp', 'order_data.xlsx')
+if not path.isfile(EXCEL):
+    urlretrieve('https://bit.ly/2JpniQ2', EXCEL)
+
+
+def load_excel_into_dataframe(excel=EXCEL):
+    """Load the SalesOrders sheet of the excel book (EXCEL variable)
+       into a Pandas DataFrame and return it to the caller"""
+    return pd.read_excel(excel, sheet_name='SalesOrders')
+
+
+def get_year_region_breakdown(df):
+    """Group the DataFrame by year and region, summing the Total
+       column. You probably need to make an extra column for
+       year, return the new df as shown in the Bite description"""
+    df['Year'] = df['OrderDate'].apply(lambda x: int(datetime.strftime(x,'%Y')))
+    grouped = df.groupby(['Year','Region'])
+    return grouped['Total'].agg(np.sum)
+
+def get_best_sales_rep(df):
+    """Return a tuple of the name of the sales rep and
+       the total of his/her sales"""
+    grouprep = df.groupby(['Rep'])
+    topsales = grouprep['Rep','Total'].agg(np.sum).sort_values(
+                                       'Total',ascending=False).head(1)
+    topsales.reset_index(level=0, inplace=True)
+    return list(topsales.itertuples(index=False))[0]
+
+
+
+def get_most_sold_item(df):
+    """Return a tuple of the name of the most sold item
+       and the number of units sold"""
+    groupbyitem = df.groupby(['Item'])
+    topitem = groupbyitem['Item','Units'].agg(np.sum).sort_values(
+                                          'Units',ascending=False).head(1)
+    topitem.reset_index(level=0, inplace=True)
+    return list(topitem.itertuples(index=False))[0]
