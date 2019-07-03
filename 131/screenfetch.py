388f491e57f3a3eba2ea9cdc4772cc6c10d20d1c
TR <thomas@reppert.info>
2019-07-03 15:43:31 -0400

    submission Bite 131 @ codechalleng.es

---
+++ 131/screenfetch.py
@@ -0,0 +1,32 @@
+output = """
+                                       mohh@SERENiTY
+ MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        OS: Mint 19 tara
+ MMm----::-://////////////oymNMd+'     Kernel: x86_64 Linux 4.15.0-34-generic
+ MMd      /++                -sNMd:    Uptime: 1d 4m
+ MMNso/'  dMM    '.::-. .-::.' .hMN:   Packages: 2351
+ ddddMMh  dMM   :hNMNMNhNMNMNh: 'NMm   Shell: zsh 5.4.2
+     NMm  dMM  .NMN/-+MMM+-/NMN' dMM   Resolution: 1366x768
+     NMm  dMM  -MMm  'MMM   dMM. dMM   DE: Cinnamon 3.8.9
+     NMm  dMM  -MMm  'MMM   dMM. dMM   WM: Muffin
+     NMm  dMM  .mmd  'mmm   yMM. dMM   WM Theme: Linux Mint (Mint-Y)
+     NMm  dMM'  ..'   ...   ydm. dMM   GTK Theme: Mint-Y [GTK2/3]
+     hMM- +MMd/-------...-:sdds  dMM   Icon Theme: Mint-Y
+     -NMm- :hNMNNNmdddddddddy/'  dMM   Font: Noto Sans 9
+      -dMNs-''-::::-------.''    dMM   CPU: AMD A10-7400P Radeon R6, 10 Compute Cores 4C+6G @ 4x 2.5GHz [101.0°C]
+       '/dMNmy+/:-------------:/yMMM   GPU: AMD KAVERI (DRM 2.50.0 / 4.15.0-34-generic, LLVM 6.0.0)
+          ./ydNMMMMMMMMMMMMMMMMMMMMM   RAM: 1886MiB / 6915MiB
+             \.MMMMMMMMMMMMMMMMMMM    
+"""
+
+
+def sysinfo_scrape(output):
+    """Scrapes the output from screenfetch and returns a dictionary"""
+    import re
+    regex = re.compile(r'(\w+\@\w+|OS:\s.*|Kernel.*|Uptime:.*|Packages:.*|Shell:.*|Resolution:.*|DE:.*|WM:.*|WM Theme:.*|GTK Theme:.*|Icon Theme:.*|Font:.*|GPU:.*|CPU:.*|RAM:.*)',re.M)
+    new_output = regex.findall(output)
+    data = {}
+    data['Name'] = new_output[0]
+    for line in new_output[1:]:
+        label, info = line.split(':')
+        data[label] = info.strip()
+    return data
