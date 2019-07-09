4fe7c9b0994c2ce16da860d05de39c420a167832
TR <thomas@reppert.info>
2019-07-08 23:05:49 -0400

    submission Bite 124 @ codechalleng.es

---
+++ 124/marvel.py
@@ -0,0 +1,64 @@
+from collections import namedtuple, Counter
+import csv
+import re
+
+import requests
+
+MARVEL_CSV = 'https://raw.githubusercontent.com/pybites/marvel_challenge/master/marvel-wikia-data.csv'  # noqa E501
+
+Character = namedtuple('Character', 'pid name sid align sex appearances year')
+
+
+# csv parsing code provided so this Bite can focus on the parsing
+
+def _get_csv_data():
+    """Download the marvel csv data and return its decoded content"""
+    with requests.Session() as session:
+        return session.get(MARVEL_CSV).content.decode('utf-8')
+
+
+def load_data():
+    """Converts marvel.csv into a sequence of Character namedtuples
+       as defined above"""
+    content = _get_csv_data()
+    reader = csv.DictReader(content.splitlines(), delimiter=',')
+    for row in reader:
+        name = re.sub(r'(.*?)\(.*', r'\1', row['name']).strip()
+        yield Character(pid=row['page_id'],
+                        name=name,
+                        sid=row['ID'],
+                        align=row['ALIGN'],
+                        sex=row['SEX'],
+                        appearances=row['APPEARANCES'],
+                        year=row['Year'])
+
+
+data = list(load_data())
+
+
+# start coding
+
+def most_popular_characters(top=5):
+    """Get the most popular character by number of appearances,
+       return top n characters (default 5)"""
+    characters = [(x.name, int(x.appearances) ) for x in data if x.appearances != '']
+    #print(characters)
+    ordered = sorted(characters, key=lambda x:x[1], reverse=True)[:top]
+    return [ x[0] for x in ordered ]
+
+
+def max_and_min_years_new_characters():
+    """Get the year with most and least new characters introduced respectively,
+       use either the 'FIRST APPEARANCE' or 'Year' column in the csv data, or
+       the 'year' attribute of the namedtuple, return a tuple of
+       (max_year, min_year)"""
+    year_counts = Counter([ x.year for x in data if x.year != ''])
+    max_year = year_counts.most_common()[0]
+    min_year = year_counts.most_common()[-1]
+    return (max_year[0], min_year[0])
+
+
+def percentage_female():
+    """Get the percentage of female characters as percentage of all characters, rounded to 2 digits"""
+    female_chars = len([ x.sex for x in data if x.sex == 'Female Characters'])
+    return round((female_chars/len(data))*100,2)
