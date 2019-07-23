b0672a976fa0baa9d0ce5f3871847736d7ab3762
Thomas Reppert <thomas@reppert.info>
2019-07-23 18:07:08 -0400

    submission Bite 203 @ codechalleng.es

--- 203/type_hinting.py
+++ 203/type_hinting.py
@@ -8,16 +8,21 @@ class Employee:
     :param wage: Float of hourly pay
     :param weekly_pay: Property which returns a string for weekly pay
     """
+    first_name: str
+    last_name: str
+    days_per_week: int
+    hours_per_day: float
+    wage: float
 
     def __init__(self, first_name: str, last_name: str, days_per_week:int,
-                 hours_per_day:float, wage:float):
+                 hours_per_day:float, wage:float) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.days_per_week = days_per_week
         self.hours_per_day = hours_per_day
         self.wage = wage
 
-    def _rounder(self, number:float, places:int) -> float:
+    def _rounder(self, number:float, places:int):
         """Rounds a number the specified number of places
 
         :param number: Float of number of round
@@ -28,7 +33,7 @@ class Employee:
         return f"${amount:0.2f}"
 
     @property
-    def weekly_pay(self) -> float:
+    def weekly_pay(self) -> str:
         """Returns amount of weekly pay in US currency
 
         For instance: $250.75
