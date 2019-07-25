7bb650f3dd0a7a983a585af2379764b9dd179083
Thomas Reppert <thomas@reppert.info>
2019-07-25 15:32:26 -0400

    submission Bite 208 @ codechalleng.es

--- 208/combos.py
+++ 208/combos.py
@@ -1,11 +1,6 @@
+from itertools import combinations
+
 def find_number_pairs(numbers, N=10):
-    pairs = []
-    pair_set = set()
-    for i in range(len(numbers)):
-        for j in range(len(numbers)):
-            if numbers[i] + numbers[j] == N and i != j:
-                if tuple(sorted([ numbers[i], numbers[j] ])) not in pair_set:
-                    pairs.append((numbers[i], numbers[j]))
-                    pair_set.add(tuple(sorted([numbers[i], numbers[j]])))
-    
-    return pairs
+    return [(i, j) for i, j in combinations(numbers, 2)
+            if i + j == N]
+
