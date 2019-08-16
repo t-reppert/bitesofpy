95f4a37d077223e7f489d86badc333cf8ca87928
Thomas Reppert <thomas@reppert.info>
2019-08-16 08:54:12 -0400

    submission Bite 209 @ codechalleng.es

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,6 +1,5 @@
 def sum_numbers(numbers):
-    """Returns a sum of numbers
-    
+    """Sums numbers
     :param numbers: a list of numbers
     :type numbers: list
     :raises TypeError: if not all numeric values passed in
