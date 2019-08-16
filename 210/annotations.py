1c3dbb72abc59f4a41f70432bcbad3624030fd4e
Thomas Reppert <thomas@reppert.info>
2019-08-16 09:19:09 -0400

    submission Bite 210 @ codechalleng.es

--- 210/annotations.py
+++ 210/annotations.py
@@ -1,9 +1,9 @@
 from typing import List
-def sum_numbers(numbers: List[float]) -> int:
+def sum_numbers(numbers: List[int]) -> int:
     """Sums numbers
 
     Args:
-        numbers (List[float]): a list of numbers
+        numbers (List[int]): a list of numbers
     
     Raises:
         TypeError: if not all numeric values passed in
