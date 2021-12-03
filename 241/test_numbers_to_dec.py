1ed45d8d9958b96cc90a531585ea7a22378cf96d
Thomas Reppert <tom.reppert@pm.me>
2021-12-03 15:11:41 -0500

    submission Bite 241 @ codechalleng.es

---
+++ 241/test_numbers_to_dec.py
@@ -0,0 +1,26 @@
+import pytest
+
+from numbers_to_dec import list_to_decimal
+
+@pytest.mark.parametrize("test_input,expected", [([4,2,8], 428)])
+def test_correct(test_input, expected):
+    result = list_to_decimal(test_input)
+    assert result == expected
+
+
+def test_boolean_error():
+    with pytest.raises(TypeError):
+        list_to_decimal([6,2,True])
+     
+def test_neg_error():
+    with pytest.raises(ValueError):
+        list_to_decimal([-3,12])
+
+def test_dec_error():
+    with pytest.raises(TypeError):
+        list_to_decimal([3.6,4,1])
+
+def test_string_error():
+    with pytest.raises(TypeError):
+        list_to_decimal(['4',5,3,1])
+
