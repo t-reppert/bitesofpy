7332a15be89027e312b21c6e01cefb35c30dd254
Thomas Reppert <tom.reppert@pm.me>
2021-12-03 15:44:53 -0500

    submission Bite 241 @ codechalleng.es

--- 241/test_numbers_to_dec.py
+++ 241/test_numbers_to_dec.py
@@ -3,16 +3,6 @@ import pytest
 from numbers_to_dec import list_to_decimal
 
 
-@pytest.mark.parametrize("test_input,expected",
-                         [([4, 2, 8], 428), 
-                          ([1, 2], 12),
-                          ([3], 3),
-                          ([1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4], 1234123412341234),
-                          ([1,2,3,4,5,6,7,8,9,0],1234567890),
-                          ])
-def test_correct(test_input, expected):
-    result = list_to_decimal(test_input)
-    assert result == expected
 
 
 def test_boolean_error():
@@ -62,3 +52,15 @@ def test_string_error2():
 def test_string_error3():
     with pytest.raises(TypeError):
         list_to_decimal([8, 6, 4, 5, 1, 7, "Ten"])
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [([4, 2, 8], 428), 
+                          ([1, 2], 12),
+                          ([3], 3),
+                          ([1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4], 1234123412341234),
+                          ([1,2,3,4,5,6,7,8,9,0],1234567890),
+                          ])
+def test_correct(test_input, expected):
+    result = list_to_decimal(test_input)
+    assert result == expected
