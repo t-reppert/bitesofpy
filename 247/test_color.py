75d913e8e23c83a3e2008c773e893a6eea220c3b
Thomas Reppert <tom.reppert@pm.me>
2021-12-13 20:04:28 -0500

    submission Bite 247 @ codechalleng.es

--- 247/test_color.py
+++ 247/test_color.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-
+import types
 import pytest
 
 import color
@@ -9,9 +9,17 @@ import color
 def gen():
     with patch('color.sample') as color_sample:
         color_sample.return_value = [101, 236, 5]
-        value = next(color.gen_hex_color())
-        return value
+        value = next(color.gen_hex_color())  #65EC05
+        return next(color.gen_hex_color())
 
 
 def test_gen_hex_color(gen):
-    assert gen == '#65EC05'
+    with patch('color.sample') as color_sample:
+        color_sample.return_value = [101, 236, 5]
+        g = color.gen_hex_color()  #65EC05
+        assert type(g) == types.GeneratorType
+        assert next(g) == '#65EC05'
+        color_sample.return_value = [236, 101, 100]
+        g = color.gen_hex_color()
+        assert type(g) == types.GeneratorType        
+        assert g == '#EC6564'
