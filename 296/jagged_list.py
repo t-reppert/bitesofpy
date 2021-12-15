c57fba8e0f052a52bf4cbea8c616a64101997f56
Thomas Reppert <tom.reppert@pm.me>
2021-12-14 23:05:23 -0500

    submission Bite 296 @ codechalleng.es

--- 296/jagged_list.py
+++ 296/jagged_list.py
@@ -2,17 +2,5 @@ from typing import List
 from itertools import zip_longest
 
 def jagged_list(lst_of_lst: List[List[int]], fillvalue: int = 0) -> List[List[int]]:
-    joined = []
-    for l in lst_of_lst:
-        temp = ""
-        for z in l:
-            temp += str(z)
-        joined.append(temp)
-    z = zip_longest(*joined, fillvalue=fillvalue)
-    filled_z = zip(*z)
-    unzipped_list = list(filled_z)
-    final = []
-    for l in unzipped_list:
-        t = [int(i) for i in l]
-        final.append(t)
-    return final
+    z = zip_longest(*lst_of_lst, fillvalue=fillvalue)
+    return [list(l) for l in zip(*z)]
