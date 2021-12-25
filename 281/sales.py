6fb50212148a41c07cc4e5e28e6a76d288b77e07
Thomas Reppert <tom.reppert@pm.me>
2021-12-25 01:14:01 -0500

    submission Bite 281 @ codechalleng.es

---
+++ 281/sales.py
@@ -0,0 +1,132 @@
+import json
+import os
+from pathlib import Path
+from typing import Dict, List, Union
+from pprint import pprint
+import pandas as pd  # type: ignore
+import requests
+from datetime import datetime
+pd.options.mode.chained_assignment = None
+
+URL: str = "https://bites-data.s3.us-east-2.amazonaws.com/MonthlySales.csv"
+STATS: List[str] = ["sum", "mean", "max"]
+TMP: Path = Path(os.getenv("TMP", "/tmp")) / "MonthlySales.csv"
+
+
+def get_data(url: str) -> Dict[str, str]:
+    """Get data from Github
+
+    Args:
+        url (str): The URL where the data is located.
+
+    Returns:
+        Dict[str, str]: The dictionary extracted from the data
+    """
+    if TMP.exists():
+        data = json.loads(TMP.read_text())
+    else:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = json.loads(response.text)
+        with TMP.open("w") as tmp:
+            json.dump(data, tmp)
+    return data
+
+
+def process_data(url: str) -> pd.DataFrame:
+    """Process the data from the Github API
+
+    Args:
+        url (str): The URL where the data is located.
+
+    Returns:
+        pd.DataFrame: Pandas DataFrame generated from the processed data
+    """
+    data = get_data(url)
+    return pd.read_csv(data['download_url'])
+
+
+def summary_report(df: pd.DataFrame, stats: Union[List[str], None] = STATS) -> None:
+    """Summary report generated from the DataFrame and list of stats
+
+    Will aggregate statistics for sum, mean, and max by default.
+
+    Args:
+        df (pd.DataFrame): Pandas DataFrame of the Github API data
+        stats (List[str], optional): List of summaries to aggregate. Defaults to STATS.
+
+    Returns:
+        None (prints to standard output)
+
+        Example:
+                    sum          mean        max
+        year
+        2013  484247.51  40353.959167   81777.35
+        2014  470532.51  39211.042500   75972.56
+        2015  608473.83  50706.152500   97237.42
+        2016  733947.03  61162.252500  118447.83
+    """
+    df['month'] = df['month'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+    df['year'] = df['month'].apply(lambda x: datetime.strftime(x, "%Y")).apply(int)
+    df.drop(columns=['month'], inplace=True)
+    group_df = df.groupby('year').sales.agg(stats)
+    print(group_df)
+
+
+def yearly_report(df: pd.DataFrame, year: int) -> None:
+    """Generate a sales report for the given year
+
+    Args:
+        df (pd.DataFrame): Pandas DataFrame of the Github API data
+        year (int): The year to generate the report for
+
+    Raises:
+        ValueError: Error raised if the year requested is not in the data.
+        Should be in the form of "The year YEAR is not included in the report!"
+
+    Returns:
+        None (prints to standard output)
+
+        Example:
+        2013
+                  sales
+        month
+        1      14236.90
+        2       4519.89
+        3      55691.01
+        4      28295.35
+        5      23648.29
+        6      34595.13
+        7      33946.39
+        8      27909.47
+        9      81777.35
+        10     31453.39
+        11     78628.72
+        12     69545.62
+    """
+    df['month'] = df['month'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+    df['year'] = df['month'].dt.year
+    if year not in df['year'].to_list():
+        raise ValueError(f"The year {year} is not included in the report!")
+    df = df[df['month'].dt.year == year ]
+    df['month'] = df['month'].apply(lambda x: datetime.strftime(x, "%-m"))
+    print()
+    print(year)
+    print(f'{"sales":>15s}')
+    print('month')
+    report = df.to_string(index=False, columns=['month', 'sales'], header=False).splitlines()
+    for line in report:
+        s = line.split()
+        if year == 2016:
+            print(f'{s[0]:<2s}{s[1]:>14s}')
+        else:
+            print(f'{s[0]:<2s}{s[1]:>13s}')
+
+#uncomment the following for viewing/testing the reports/code
+#if __name__ == "__main__":
+    #data = process_data(URL)
+    #summary_report(data)
+    #for year in (data["month"].dt.year).unique():
+    #   yearly_report(data, year)
+
+    #yearly_report(data, 2016)
