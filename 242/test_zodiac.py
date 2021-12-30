d197a6e8032d24914474c21d653939c8808d8b62
Thomas Reppert <tom.reppert@pm.me>
2021-12-30 15:13:43 -0500

    submission Bite 242 @ codechalleng.es

---
+++ 242/test_zodiac.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import json
+import os
+from pathlib import Path
+from urllib.request import urlretrieve
+
+import pytest
+
+from zodiac import (get_signs, get_sign_with_most_famous_people,
+                    signs_are_mutually_compatible, get_sign_by_date)
+
+# original source: https://zodiacal.herokuapp.com/api
+URL = "https://bites-data.s3.us-east-2.amazonaws.com/zodiac.json"
+TMP = os.getenv("TMP", "/tmp")
+PATH = Path(TMP, "zodiac.json")
+
+
+@pytest.fixture(scope='module')
+def signs():
+    if not PATH.exists():
+        urlretrieve(URL, PATH)
+    with open(PATH) as f:
+        data = json.loads(f.read())
+    return get_signs(data)
+
+def test_get_sign_with_most_people(signs):
+    actual = get_sign_with_most_famous_people(signs)
+    assert actual == ('Scorpio', 35)
+    assert actual != ('Gemini', 15)
+
+@pytest.mark.parametrize('sign1,sign2,expected',[
+    ('Gemini','Libra', True),
+    ('Taurus','Capricorn', True),
+    ('Gemini','Pisces', False),
+    ('Aries','Scorpio', False),
+
+])
+def test_signs_are_mutually_compatible(signs, sign1, sign2, expected):
+    assert signs_are_mutually_compatible(signs,sign1,sign2) == expected
+
+
+def test_get_sign_by_date(signs):
+    date = datetime(year=2021, month=6, day=13)
+    actual = get_sign_by_date(signs, date)
+    assert actual == 'Gemini'
+    date = datetime(year=2021, month=1, day=15)
+    actual = get_sign_by_date(signs, date)
+    assert actual == 'Capricorn'
+
+
