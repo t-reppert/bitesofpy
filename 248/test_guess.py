39fa8990aaea97ff79392f0a8487206bdfe26aba
Thomas Reppert <tom.reppert@pm.me>
2021-12-30 18:34:41 -0500

    submission Bite 248 @ codechalleng.es

---
+++ 248/test_guess.py
@@ -0,0 +1,58 @@
+from unittest.mock import patch, MagicMock
+
+import sys
+import pytest
+
+from guess import GuessGame, InvalidNumber, MAX_NUMBER
+
+
+# write test code to reach 100% coverage and a 100% mutpy score
+
+def test_invalid_secret_numbers():
+    with pytest.raises(InvalidNumber, match='Not a number'):
+        GuessGame('haha')
+    with pytest.raises(InvalidNumber, match='Negative number'):
+        GuessGame(-50)
+    with pytest.raises(InvalidNumber, match='Number too high'):
+        GuessGame(MAX_NUMBER+50)
+
+def test_valid_secret_numbers(monkeypatch, capsys):
+    g = GuessGame(9)
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda: 9)
+        g()
+        captured = capsys.readouterr()
+        assert "You guessed it!\n" in captured.out
+    
+
+def test_too_low(monkeypatch, capsys):
+    g = GuessGame(9)
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda: 5)
+        g()
+        captured = capsys.readouterr()
+        assert "Too low\n" in captured.out
+
+def test_too_high(monkeypatch, capsys):
+    g = GuessGame(9)
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda: 12)
+        g()
+        captured = capsys.readouterr()
+        assert "Too high\n" in captured.out
+
+def test_max_guesses(monkeypatch, capsys):
+    g = GuessGame(9)
+    with monkeypatch.context() as m:
+        m.setattr('builtins.input', lambda: 12)
+        g()
+        m.setattr('builtins.input', lambda: 12)
+        g()
+        m.setattr('builtins.input', lambda: 12)
+        g()
+        m.setattr('builtins.input', lambda: 12)
+        g()
+        m.setattr('builtins.input', lambda: 12)
+        g()
+        captured = capsys.readouterr()
+        assert f"Sorry, the number was 9\n" in captured.out
