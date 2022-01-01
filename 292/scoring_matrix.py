4e53dc33143d23cde66bafa57d4ea64dda45265f
Thomas Reppert <tom.reppert@pm.me>
2022-01-01 17:21:08 -0500

    submission Bite 292 @ codechalleng.es

---
+++ 292/scoring_matrix.py
@@ -0,0 +1,86 @@
+from typing import Union, List
+import pandas as pd
+
+# Grabbed matrix from from
+# https://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt
+# Fixed width of 3
+BLOSUM62 = """#  Matrix made by matblas from blosum62.iij
+#  * column uses minimum score
+#  BLOSUM Clustered Scoring Matrix in 1/2 Bit Units
+#  Blocks Database = /data/blocks_5.0/blocks.dat
+#  Cluster Percentage: >= 62
+#  Entropy =   0.6979, Expected =  -0.5209
+   A  R  N  D  C  Q  E  G  H  I  L  K  M  F  P  S  T  W  Y  V  B  Z  X  *
+A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1  0 -4
+R -1  5  0 -2 -3  1  0 -2  0 -3 -2  2 -1 -3 -2 -1 -1 -3 -2 -3 -1  0 -1 -4
+N -2  0  6  1 -3  0  0  0  1 -3 -3  0 -2 -3 -2  1  0 -4 -2 -3  3  0 -1 -4
+D -2 -2  1  6 -3  0  2 -1 -1 -3 -4 -1 -3 -3 -1  0 -1 -4 -3 -3  4  1 -1 -4
+C  0 -3 -3 -3  9 -3 -4 -3 -3 -1 -1 -3 -1 -2 -3 -1 -1 -2 -2 -1 -3 -3 -2 -4
+Q -1  1  0  0 -3  5  2 -2  0 -3 -2  1  0 -3 -1  0 -1 -2 -1 -2  0  3 -1 -4
+E -1  0  0  2 -4  2  5 -2  0 -3 -3  1 -2 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4
+G  0 -2  0 -1 -3 -2 -2  6 -2 -4 -4 -2 -3 -3 -2  0 -2 -2 -3 -3 -1 -2 -1 -4
+H -2  0  1 -1 -3  0  0 -2  8 -3 -3 -1 -2 -1 -2 -1 -2 -2  2 -3  0  0 -1 -4
+I -1 -3 -3 -3 -1 -3 -3 -4 -3  4  2 -3  1  0 -3 -2 -1 -3 -1  3 -3 -3 -1 -4
+L -1 -2 -3 -4 -1 -2 -3 -4 -3  2  4 -2  2  0 -3 -2 -1 -2 -1  1 -4 -3 -1 -4
+K -1  2  0 -1 -3  1  1 -2 -1 -3 -2  5 -1 -3 -1  0 -1 -3 -2 -2  0  1 -1 -4
+M -1 -1 -2 -3 -1  0 -2 -3 -2  1  2 -1  5  0 -2 -1 -1 -1 -1  1 -3 -1 -1 -4
+F -2 -3 -3 -3 -2 -3 -3 -3 -1  0  0 -3  0  6 -4 -2 -2  1  3 -1 -3 -3 -1 -4
+P -1 -2 -2 -1 -3 -1 -1 -2 -2 -3 -3 -1 -2 -4  7 -1 -1 -4 -3 -2 -2 -1 -2 -4
+S  1 -1  1  0 -1  0  0  0 -1 -2 -2  0 -1 -2 -1  4  1 -3 -2 -2  0  0  0 -4
+T  0 -1  0 -1 -1 -1 -1 -2 -2 -1 -1 -1 -1 -2 -1  1  5 -2 -2  0 -1 -1  0 -4
+W -3 -3 -4 -4 -2 -2 -3 -2 -2 -3 -2 -3 -1  1 -4 -3 -2 11  2 -3 -4 -3 -2 -4
+Y -2 -2 -2 -3 -2 -1 -2 -3  2 -1 -1 -2 -1  3 -3 -2 -2  2  7 -1 -3 -2 -1 -4
+V  0 -3 -3 -3 -1 -2 -2 -3 -3  3  1 -2  1 -1 -2 -2  0 -3 -1  4 -3 -2 -1 -4
+B -2 -1  3  4 -3  0  1 -1  0 -3 -4  0 -3 -3 -2  0 -1 -4 -3 -3  4  1 -1 -4
+Z -1  0  0  1 -3  3  4 -2  0 -3 -3  1 -1 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4
+X  0 -1 -1 -1 -2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -2  0  0 -2 -1 -1 -1 -1 -1 -4
+* -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4  1 """
+
+class AminoAcidNotFoundError(Exception):
+    pass
+
+
+def matrix_score(sequence1: str, sequence2: str, matrix_str: str = BLOSUM62) -> int:
+    """
+    Receives two proteins sequences and a matrix table
+    Returns the score of two proteins according to the supplied matrix table
+    """
+    scores = []
+    df = matrix_to_df(matrix_str)
+    s1 = [char.upper() for char in sequence1]
+    s2 = [char.upper() for char in sequence2]
+    for idx, s in enumerate(s1):
+        try:
+            score = df.loc[s, s2[idx]]
+        except KeyError:
+            raise AminoAcidNotFoundError(f"Scoring matrix does not support scoring for: ('{s}', '{s2[idx]}')")
+        scores.append(int(score))
+    return sum(scores)
+
+
+def closest_match(
+    reference_sequence: str, query_sequences: List[str], matrix_str: str = BLOSUM62
+) -> Union[str, List, None]:
+    """
+    Receives a reference sequence, a list of query sequences and a matrix table
+    Returns the closest matching sequence(s) or None
+    """
+    if not query_sequences:
+        return None
+    sequence_scores = [(idx, matrix_score(reference_sequence, s, matrix_str)) for idx,s in enumerate(query_sequences)]
+    closest_matching = max(sequence_scores, key=lambda x: x[1])
+    ties = [s for s in sequence_scores if closest_matching[1] == s[1]]
+    if len(ties) > 1:
+        return [query_sequences[tie[0]] for tie in ties]
+    return query_sequences[closest_matching[0]]
+
+
+def matrix_to_df(matrix_str) -> pd.DataFrame:
+    raw_matrix = [line for line in matrix_str.splitlines() if not line.startswith("#")]
+    new_matrix = []
+    columns = raw_matrix[0].strip().split()
+    for line in raw_matrix[1:]:
+        line_split = line.split()
+        new_matrix.append(line_split[1:])
+    df = pd.DataFrame(new_matrix, columns=columns, index=columns)
+    return df
