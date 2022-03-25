76f9f383f3fe2dd8470f1508c8a385dc07025e82
Thomas Reppert <tom.reppert@pm.me>
2022-03-25 16:43:26 -0400

    submission Bite 341 @ codechalleng.es

---
+++ 341/models.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from typing import Any
+
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+# https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
+# which we'll further explore in a later Bite
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+class Food(BaseModel):
+    """Bite 02"""
+
+    id: int
+    name: str
+    serving_size: str
+    kcal_per_serving: int
+    protein_grams: float
+    fibre_grams: float = 0
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    password: str
+
+    def __init__(self, **data):
+        data["password"] = get_password_hash(data["password"])
+        super().__init__(**data)
+
+
+class FoodEntry(BaseModel):
+    id: int
+    user: User
+    food: Food
+    date_added: datetime = datetime.now()
+    number_servings: float
+
+    @property
+    def total_calories(self):
+        return self.number_servings * self.food.kcal_per_serving
+
